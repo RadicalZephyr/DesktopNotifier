15a9c0c18e2750c8f749e669c1e9fc9db349e8a5
Geoff Shannon <earthlingzephyr@gmail.com>
2016-12-24 22:13:39 -0600

    Get basic notification working.

--- DesktopNotifier/Program.cs
+++ DesktopNotifier/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace DesktopNotifier
 {
@@ -8,6 +10,13 @@ namespace DesktopNotifier
     {
         static void Main(string[] args)
         {
+
+            var notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = new Icon("C:\\Users\\Geoff\\boot-logo-3.ico");
+            notifyIcon.BalloonTipTitle = "Boot";
+            notifyIcon.BalloonTipText = "Hello, World!";
+            notifyIcon.Visible = true;
+            notifyIcon.ShowBalloonTip(10000);
         }
     }
 }
